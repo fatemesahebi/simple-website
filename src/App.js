353b3fbcdbe1254d11bc7de849a07d36461e565c
fateme <f.sahebi.fs@gmail.com>
2022-01-17 01:11:24 +0330

    github page setting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import {Menu, Header, Team, Person, imageList, ContactUs} from './components'
-import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import {Route, Routes,HashRouter} from 'react-router-dom'
 import {useState} from "react";
 
 function App() {
     const [page,setPage]=useState(0)
     return (
-  <BrowserRouter>
+  <HashRouter>
       <Routes>
           <Route path="/" element={<Menu page={page} setPage={setPage}/>}>
           <Route index element={<Header page={page} setPage={setPage}/>}/>
@@ -15,7 +15,7 @@ function App() {
               <Route path='/contact-us' element={<ContactUs page={page} setPage={setPage}/>} />
           </Route>
       </Routes>
-  </BrowserRouter>
+  </HashRouter>
   );
 }
 
